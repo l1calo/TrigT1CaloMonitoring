2031b3bb5b44383abaf4b3458c9a96a9b5928ef7
woehrlin <woehrlin@4525493e-7705-40b1-a816-d608a930855b>
2007-01-18 16:01:10 +0000

    added compiling BS code
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/Trigger/TrigT1/TrigT1CaloMonitoring/trunk@171101 4525493e-7705-40b1-a816-d608a930855b

--- src/components/TrigT1CaloMonitoring_entries.cxx
+++ src/components/TrigT1CaloMonitoring_entries.cxx
@@ -1,10 +1,13 @@
 #include "TrigT1CaloMonitoring/TrigT1CaloMonTool.h"
+#include "TrigT1CaloMonitoring/TrigT1CaloBSMonTool.h"
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_TOOL_FACTORY(TrigT1CaloMonTool )
+DECLARE_TOOL_FACTORY(TrigT1CaloBSMonTool )
 
 DECLARE_FACTORY_ENTRIES(TrigT1CaloMonitoring) {
   DECLARE_ALGTOOL(TrigT1CaloMonTool )
+  DECLARE_ALGTOOL(TrigT1CaloBSMonTool )
 
 }
 
