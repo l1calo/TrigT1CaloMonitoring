9d84e8d0f9ffaa17336b1c4bd7fe0b42f297d322
pjfaulkn <pjfaulkn@4525493e-7705-40b1-a816-d608a930855b>
2007-05-16 16:36:04 +0000

    Update requirements and _entries.cxx for CPM
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/Trigger/TrigT1/TrigT1CaloMonitoring/trunk@171125 4525493e-7705-40b1-a816-d608a930855b

--- src/components/TrigT1CaloMonitoring_entries.cxx
+++ src/components/TrigT1CaloMonitoring_entries.cxx
@@ -1,5 +1,6 @@
 #include "TrigT1CaloMonitoring/TrigT1CaloMonTool.h"
 #include "TrigT1CaloMonitoring/TrigT1CaloBSMonTool.h"
+#include "TrigT1CaloMonitoring/TrigT1CaloCpmMonTool.h"
 #include "TrigT1CaloMonitoring/JetElementMon.h"
 #include "TrigT1CaloMonitoring/JEMMon.h"
 #include "TrigT1CaloMonitoring/CMMMon.h"
@@ -8,6 +9,7 @@
 
 DECLARE_TOOL_FACTORY(TrigT1CaloMonTool )
 DECLARE_TOOL_FACTORY(TrigT1CaloBSMonTool )
+DECLARE_TOOL_FACTORY(TrigT1CaloCpmMonTool )
 DECLARE_TOOL_FACTORY(JetElementMon);
 DECLARE_TOOL_FACTORY(JEMMon);
 DECLARE_TOOL_FACTORY(CMMMon);
@@ -15,6 +17,7 @@ DECLARE_TOOL_FACTORY(CMMMon);
 DECLARE_FACTORY_ENTRIES(TrigT1CaloMonitoring) {
   DECLARE_ALGTOOL(TrigT1CaloMonTool )
   DECLARE_ALGTOOL(TrigT1CaloBSMonTool )
+  DECLARE_ALGTOOL(TrigT1CaloCpmMonTool )
   DECLARE_ALGTOOL(JetElementMon);
   DECLARE_ALGTOOL(JEMMon);
   DECLARE_ALGTOOL(CMMMon);
