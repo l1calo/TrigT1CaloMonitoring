ff1c3bd3c256659d7e328c1d2105f06fa4dfdfd0
fleckner <fleckner@4525493e-7705-40b1-a816-d608a930855b>
2007-08-24 19:45:30 +0000

    JO update for AthenaPT
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/Trigger/TrigT1/TrigT1CaloMonitoring/trunk@171216 4525493e-7705-40b1-a816-d608a930855b

--- share/TrigT1CaloMonitoring_AthenaPT_topOptions.py
+++ share/TrigT1CaloMonitoring_AthenaPT_topOptions.py
@@ -2,63 +2,50 @@
 # TopOptions to execute RecExCommissioning in PT
 #               (and provide data for online event display)
 #
-# 21/08/2007  Jiri.Masik@cern.ch
 ######################################################################
 
 #default flags
 include("RecExCommission/RecExCommissionFlags_jobOptions.py")
 
-ATLASCosmicFlags.EvtMax = 10
+#important
+ATLASCosmicFlags.doOnline=True
+ATLASCosmicFlags.doMonitoring=True
+ATLASCosmicFlags.doLVL1Calo=True
+ATLASCosmicFlags.doLVL1CaloCPJEPMonitoring=True
 
-#crucial
+CBNTAthenaAware = False
 ATLASCosmicFlags.doSim=False
 ATLASCosmicFlags.JiveXML=False
-ATLASCosmicFlags.doOnline=False
-
-#optional switches
-ATLASCosmicFlags.doMonitoring=False
-ATLASCosmicFlags.doLVL1Calo=True
-
 ATLASCosmicFlags.doInDet=False
 ATLASCosmicFlags.doMuons=False
 ATLASCosmicFlags.doLAr=False
 ATLASCosmicFlags.doTile=False
 ATLASCosmicFlags.doCaloTrkMuId=False
-ATLASCosmicFlags.doLVL1CaloCPJEPMonitoring=False
 ATLASCosmicFlags.doHLT=False
 ATLASCosmicFlags.doCTPMon=False
+ATLASCosmicFlags.OnlineJiveXML = False
+ATLASCosmicFlags.doNtuple = False
 
+
+#
+if ATLASCosmicFlags.doOnline:
+    include.block("ByteStreamCnvSvc/BSEventStorageEventSelector_jobOptions.py")
+
+# 
 include( "RecExCommission/RecExCommission_topOptions.py" )
 
-#Settuing up the monitoring infrastructure
-from AthenaMonitoring.AthenaMonitoringConf import AthenaMonManager
-topSequence += AthenaMonManager( "ManagedAthenaMonPilot" )
-ManagedAthenaMonPilot = topSequence.ManagedAthenaMonPilot
-ManagedAthenaMonPilot.FileKey             = "GLOBAL"
-#FIXME: The FileKey is not propagated to the following MonManagers.
-#FIXME: We have to make sure that the subdetector fragements use the same FileKey
-#FIXME: alternavely, we could add all the FileKeys with more  THistSvc.Output +=[..] statements.
-ManagedAthenaMonPilot.ManualDataTypeSetup = True
-ManagedAthenaMonPilot.DataType            = "cosmics"
-##  if doOnline:
-##      ManagedAthenaMonPilot.Environment     = "online"
-##  else:
-ManagedAthenaMonPilot.Environment         = "tier0"
-ManagedAthenaMonPilot.ManualRunLBSetup    = False
-#ManagedAthenaMonPilot.Run                = 1
-#ManagedAthenaMonPilot.LumiBlock          = 1
 
-include ( "TrigT1CaloMonitoring/TrigT1CaloMonitoring_forAthenaPT.py")
-toolSvc.L1TriggerTowerMonTool.DistPerChannel = False
+##
 
+#MuonCalib_MdtCalibDbAsciiTool.RT_InputFiles = [ "RT_default_comm.dat" ]
+#ServiceMgr.ExceptionSvc.Catch="none"
+try:
+    ByteStreamAddressProviderSvc.TypeNames.remove("LArCellIDC/LArCellIDC")
+except ValueError:
+    pass
+    
+MessageSvc.defaultLimit=10000
+Service( "ROBDataProviderSvc" ).filterEmptyROB=True
 
-#-----------------------------------------------------------------------------
-# Monitoring output
-#-----------------------------------------------------------------------------
-theApp.Dlls += [ "GaudiSvc" ]
-THistSvc = Algorithm( "THistSvc" )
-#    if os.path.exists(RootHistOutputFilename):
-#        os.remove(RootHistOutputFilename)
-#THistSvc.Output += [MonitorOutput+" DATAFILE='"+ATLASCosmicFlags.MonOutputFile+"'OPT='NEW'"]
-#THistSvc.Output += ["SHIFT DATAFILE='"+ATLASCosmicFlags.MonOutputFile+"'OPT='NEW'"]
-THistSvc.Output += ["GLOBAL DATAFILE='"+ATLASCosmicFlags.MonOutputFile+"'OPT='RECREATE'"]
+#to avoid crash in SCT monit
+#ByteStreamCnvSvc.InitCnvs += [ "SCT_RDO_Container","InDetRawDataCollection<SCT_RDORawData>"]
